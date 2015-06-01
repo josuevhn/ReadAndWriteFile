91344464a6964c418859dfa829d8f96e436d7549
josuevhn <josuevhn@wiseratel.com>
2015-06-01 06:30:06 -0500

    The code was developed and some new comments were added

--- ReadFile/Program.cs
+++ ReadFile/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+
 namespace ReadFile
 {
     class Program
@@ -11,7 +13,36 @@ namespace ReadFile
         static void Main(string[] args)
         {
 
+            StreamReader myReader = new StreamReader("Files/myfile.txt");
 
+            // This variable will store each line of the file.
+            String line = "";
+
+            // An empty line at the beginning of the output.
+            Console.WriteLine();
+
+            while(line != null)
+            {
+
+                line = myReader.ReadLine();
+
+                if(line != null)
+                {
+
+                    Console.WriteLine(line);
+
+                } // if
+
+            } // while
+
+            // Clossing the stream.
+            myReader.Close();
+
+            // An empty line at the end of the output.
+            Console.WriteLine();
+
+            // Wait for the user to press any key.
+            Console.ReadLine();
 
         } // Main
 
