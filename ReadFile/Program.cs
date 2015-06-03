0648d009b393a95471bb7e8228e0e82af71aa9a5
josuevhn <josuevhn@wiseratel.com>
2015-06-03 04:36:41 -0500

    Functionalities for reading and writing were added

--- ReadFile/Program.cs
+++ ReadFile/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// Libreria necesaria para utilizar StreamReader.
+// Libreria necesaria para utilizar StreamReader y StreamWriter.
 using System.IO;
 
 namespace ReadFile
@@ -14,38 +14,63 @@ namespace ReadFile
         static void Main(string[] args)
         {
 
+            // Leer Archivo
+
             // Creamos una instancia de StreamReader.
-            StreamReader myReader = new StreamReader("Files/myfile.txt");
-
-            // Esta variable almacenará cada línea del archivo.
-            String line = "";
-
-            // Línea en blanco al inicio de la salida en pantalla.
-            Console.WriteLine();
-
-            // Iteramos sobre cada línea del archivo hasta llegar al final del mismo.
-            while(line != null)
+            using (StreamReader myReader = new StreamReader("Files/myfile.txt"))
             {
 
-                // Leemos la línea a la que apunta nuestro objeto myReader.
-                line = myReader.ReadLine();
+                // Esta variable almacenará el texto a escribir.
+                String line = "";
 
-                // Comprobamos que no hemos llegado al final del archivo.
-                if(line != null)
+                // Línea en blanco.
+                Console.WriteLine();
+
+                Console.WriteLine("Reading Text:");
+
+                // Línea en blanco.
+                Console.WriteLine();
+
+                // Iteramos sobre cada línea del archivo hasta llegar al final del mismo.
+
+                do
                 {
+
+                    // Leemos la línea a la que apunta nuestro objeto myReader.
+                    line = myReader.ReadLine();
 
                     // Imprimimos la línea en pantalla.
                     Console.WriteLine(line);
 
-                } // if
+                } while (line != null);
 
-            } // while
+            } // using
 
-            // Cerramos el flujo al arhcivo.
-            myReader.Close();
+            // Escribir Archivo
 
-            // Línea en blanco al final de la salida en pantalla.
-            Console.WriteLine();
+            using(StreamWriter streamWriter = new StreamWriter("Files/myfile.txt", false))
+            {
+
+                // Línea en blanco.
+                Console.WriteLine();
+
+                Console.WriteLine("Writing Text:");
+
+                // Esta variable almacenará cada línea del archivo.
+                String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ac odio nibh. Nam sodales diam sed dolor tempor, at viverra est ultrices. Nunc justo tortor, euismod suscipit rutrum ac, tempus sed orci. Vivamus facilisis neque ac diam sagittis venenatis. Pellentesque ut justo vel eros consectetur volutpat ut sed felis. Cras tristique velit purus, vel commodo nibh finibus at. Aliquam vitae ligula orci. Mauris ullamcorper iaculis felis, at rutrum metus. Duis eget ultrices eros. Cras dapibus, lacus ut feugiat cursus, arcu odio condimentum neque, non rhoncus libero ligula ac augue. Proin vitae semper ex. Integer mollis pellentesque aliquam.";
+
+                // Línea en blanco.
+                Console.WriteLine();
+
+                Console.WriteLine(text);
+
+                // Línea en blanco.
+                Console.WriteLine();
+
+                // Escribimos texto hacia el archivo.
+                streamWriter.WriteLine(text);
+
+            } // using
 
             // Esperamos a que el usuario presione cualquier tecla.
             Console.ReadLine();
